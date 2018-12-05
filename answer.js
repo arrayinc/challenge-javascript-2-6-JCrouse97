8b0c4d510fe7a993abbc8605e3d70c0837ee021c
JCrouse97 <jcrouse@thearrayschool.com>
2018-12-05 11:46:47 -0700

    challenge complete

--- answer.js
+++ answer.js
@@ -1,1 +1,10 @@
 // *** YOUR ANSWER BELOW ***
+
+const monthsInYear = 12;
+
+function yearsAndMonthsToSeconds(x,y){
+  let totalMonths = x * monthsInYear + y;
+  return totalMonths * 2.628e+6 + " seconds";
+}
+
+yearsAndMonthsToSeconds(1,5);
